9d0602271ea563c806ff1af6cffc0214926b4a76
HamZhiWei33 <149857735+HamZhiWei33@users.noreply.github.com>
2024-04-30 00:00:09 +0800

    create a class called students but it is just a draft haha

--- src/ds/assignment/DatabaseConfig.java
+++ src/ds/assignment/DatabaseConfig.java
@@ -2,10 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-<<<<<<< HEAD
-=======
+
 package ds.assignment;
->>>>>>> 028ac6c5ae2991f33f1f0159f5d41439473dc754
 
 /**
  *
@@ -14,11 +12,7 @@ package ds.assignment;
 public class DatabaseConfig {
     public static final String URL = "jdbc:mysql://localhost:3006/datastructure";
     public static final String USERNAME = "root";
-<<<<<<< HEAD
     public static final String PASSWORD = "Mp2023@350#"; //hello
-=======
-    public static final String PASSWORD = "Mp2023@350#"; 
-    
+
     // Line for testing
->>>>>>> 028ac6c5ae2991f33f1f0159f5d41439473dc754
 }
