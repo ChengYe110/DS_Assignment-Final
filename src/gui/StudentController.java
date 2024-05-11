63b02c9908448bef16ca980cf2d993329e0efc5c
YEWJINGEN915 <147475018+YEWJINGEN915@users.noreply.github.com>
2024-05-11 08:13:48 +0800

    new update

--- src/gui/StudentController.java
+++ src/gui/StudentController.java
@@ -20,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
@@ -42,17 +43,20 @@ public class StudentController implements Initializable {
     @FXML
     private StackPane stackPane;
     @FXML
-    private Button DiscussionPage, EventPage, HomePage, LeaderboardPage, MenuButton, ProfilePage, QuizPage, CreateQuizPage, DoneCreateQuiz, DoneCreateEvent, CreateEventPage;
+    private Button DiscussionPage, EventPage, HomePage, LeaderboardPage, MenuButton, ProfilePage, QuizPage, 
+            CreateQuizPage, DoneCreateQuiz, DoneCreateEvent, CreateEventPage, FriendListPage, ExitFriendListPage,
+            FriendRequestPage,ExitFriendRequestPage;
     @FXML
-    private VBox DrawerPane;
+    private VBox DrawerPane,FriendListVBox,FriendRequestVBox;
     @FXML
-    private AnchorPane MenuPane, TopPane, HomePagePane, LeaderboardPane, QuizPane, CreateQuizPane, EventPane, CreateEventPane;
+    private AnchorPane MenuPane, TopPane, HomePagePane, LeaderboardPane, QuizPane, CreateQuizPane, EventPane, 
+            CreateEventPane, BookingPane, StudentProfilePane,FriendListPane,FriendRequestPane;
     @FXML
-    private Text UsernameText;
+    private Text UsernameText, Suggested1, Suggested2, Suggested3, Suggested4, Suggested5, Distance1, Distance2, Distance3, Distance4, Distance5;
     @FXML
-    private TextArea QuizTitleField, QuizDescriptionField, QuizContentField, EventTitleField, EventDescriptionField, EventVenueField, EventTimeField;
+    private TextArea QuizTitleField, QuizDescriptionField, QuizContentField, EventTitleField, EventDescriptionField, EventVenueField, EventTimeField, DestinationIDField;
     @FXML
-    private ChoiceBox<String> FilterChoiceBox, QuizThemeChoiceBox;
+    private ChoiceBox<String> FilterChoiceBox, QuizThemeChoiceBox, AvailableTimeSlotChoiceBox;
     @FXML
     private DatePicker EventDatePicker;
     @FXML
@@ -61,6 +65,8 @@ public class StudentController implements Initializable {
     private ObservableList<String> theme = FXCollections.observableArrayList("SCIENCE", "TECHNOLOGY", "ENGINEERING", "MATHEMATIC");
     @FXML
     private TableView<?> QuizTable;
+    @FXML
+    private ScrollPane FriendListScrollPane,FriendRequestScrollPane;
 
     private TranslateTransition slideOutTransition, slideInTransition;
     private Stage stage;
@@ -98,75 +104,89 @@ public class StudentController implements Initializable {
             FilterChoiceBox.setValue("FILTER");
             QuizThemeChoiceBox.setItems(theme);
 
+            ButtonEffect(FriendListPage);
             ProfilePage.setOnAction(event -> {
-                // Handle Profile button click
+                if (MenuPane.getTranslateX() == 0) {
+                    slideInTransition.play();
+                }
+                stackPane.getChildren().clear();
+                stackPane.getChildren().add(StudentProfilePane);
+                FriendListPane.setVisible(false);
+                FriendRequestPane.setVisible(false);
+            });
+            
+            FriendListScrollPane.setContent(FriendListVBox);
+            FriendListPage.setOnAction(event -> {
+                FriendListPane.setVisible(true);
+            });
+            
+            ButtonEffect(ExitFriendListPage);
+            ExitFriendListPage.setOnAction(event -> {
+                FriendListPane.setVisible(false);
+            });
+            
+            FriendRequestScrollPane.setContent(FriendRequestVBox);
+            FriendRequestPage.setOnAction(event -> {
+                FriendRequestPane.setVisible(true);
+            });
+            
+            ButtonEffect(ExitFriendRequestPage);
+            ExitFriendRequestPage.setOnAction(event -> {
+                FriendRequestPane.setVisible(false);
             });
 
             DiscussionPage.setOnAction(event -> {
                 // Handle Discussion button click
             });
 
-//            QuizPage.setOnAction(event -> {
-//                LeaderboardPane.setVisible(false);
-//                HomePagePane.setVisible(false);
-//                QuizPane.setVisible(true);
-//                CreateQuizPane.setVisible(false);
-//                EventPane.setVisible(false);
-//                CreateEventPane.setVisible(false);
-//                QuizPane.toFront();
-//            });
-//            FilterComboBox.getItems().addAll(theme);
-//
-//            CreateQuizPage.setOnAction(event -> {
-//                LeaderboardPane.setVisible(false);
-//                HomePagePane.setVisible(false);
-//                QuizPane.setVisible(false);
-//                CreateQuizPane.setVisible(true);
-//                EventPane.setVisible(false);
-//                CreateEventPane.setVisible(false);
-//                CreateQuizPane.toFront();
-//            });
-//            QuizThemeComboBox.getItems().addAll(theme);
-//
-//            DoneCreateQuiz.setOnAction(event -> {
-//                LeaderboardPane.setVisible(false);
-//                HomePagePane.setVisible(false);
-//                QuizPane.setVisible(true);
-//                CreateQuizPane.setVisible(false);
-//                EventPane.setVisible(false);
-//                CreateEventPane.setVisible(false);
-//                QuizPane.toFront();
-//            });
-//
-//            EventPage.setOnAction(event -> {
-//                LeaderboardPane.setVisible(false);
-//                HomePagePane.setVisible(false);
-//                QuizPane.setVisible(false);
-//                CreateQuizPane.setVisible(false);
-//                EventPane.setVisible(true);
-//                CreateEventPane.setVisible(false);
-//                EventPane.toFront();
-//            });
-//
-//            CreateEventPage.setOnAction(event -> {
-//                LeaderboardPane.setVisible(false);
-//                HomePagePane.setVisible(false);
-//                QuizPane.setVisible(false);
-//                CreateQuizPane.setVisible(false);
-//                EventPane.setVisible(false);
-//                CreateEventPane.setVisible(true);
-//                CreateEventPane.toFront();
-//            });
-//
-//            DoneCreateEvent.setOnAction(event -> {
-//                LeaderboardPane.setVisible(false);
-//                HomePagePane.setVisible(false);
-//                QuizPane.setVisible(false);
-//                CreateQuizPane.setVisible(false);
-//                EventPane.setVisible(true);
-//                CreateEventPane.setVisible(false);
-//                EventPane.toFront();
-//            });
+            QuizPage.setOnAction(event -> {
+                if (MenuPane.getTranslateX() == 0) {
+                    slideInTransition.play();
+                }
+                stackPane.getChildren().clear();
+                stackPane.getChildren().add(QuizPane);
+            });
+
+            CreateQuizPage.setOnAction(event -> {
+                if (MenuPane.getTranslateX() == 0) {
+                    slideInTransition.play();
+                }
+                stackPane.getChildren().clear();
+                stackPane.getChildren().add(CreateQuizPane);
+            });
+
+            DoneCreateQuiz.setOnAction(event -> {
+                if (MenuPane.getTranslateX() == 0) {
+                    slideInTransition.play();
+                }
+                stackPane.getChildren().clear();
+                stackPane.getChildren().add(QuizPane);
+            });
+
+            EventPage.setOnAction(event -> {
+                if (MenuPane.getTranslateX() == 0) {
+                    slideInTransition.play();
+                }
+                stackPane.getChildren().clear();
+                stackPane.getChildren().add(EventPane);
+            });
+
+            CreateEventPage.setOnAction(event -> {
+                if (MenuPane.getTranslateX() == 0) {
+                    slideInTransition.play();
+                }
+                stackPane.getChildren().clear();
+                stackPane.getChildren().add(CreateEventPane);
+            });
+
+            DoneCreateEvent.setOnAction(event -> {
+                if (MenuPane.getTranslateX() == 0) {
+                    slideInTransition.play();
+                }
+                stackPane.getChildren().clear();
+                stackPane.getChildren().add(EventPane);
+            });
+
             for (Node node : MENU.getChildren()) {
                 ((Button) node).setOnAction(event -> {
                     Button btn = (Button) event.getSource();
@@ -184,9 +204,28 @@ public class StudentController implements Initializable {
                     }
                 });
             }
-            selectedButton = (Button) ((HBox) MENU.getChildren().get(0)).getChildren().get(0);
+            selectedButton = (Button) MENU.getChildren().get(0);
             selectedButton.setId("selected");
         });
+        addFriendButton("Cindy");
+        addFriendButton("Cindy");
+        addFriendButton("Cindy");
+        addFriendButton("Cindy");
+        addFriendButton("Cindy");
+        addFriendButton("Cindy");
+        addFriendButton("Cindy");
+        addFriendButton("Jack");
+        addFriendButton("Leo");
+        addFriendRequest("Jane");
+        addFriendRequest("Jane");
+        addFriendRequest("Jane");
+        addFriendRequest("Jane");
+        addFriendRequest("Jane");
+        addFriendRequest("Johnny Dep");
+        addFriendRequest("Johnny Dep");
+        addFriendRequest("Johnny Dep");
+        addFriendRequest("Johnny Dep");
+        addFriendRequest("Johnny Dep");
     }
 
     public void switchHomePage() {
@@ -205,38 +244,46 @@ public class StudentController implements Initializable {
         stackPane.getChildren().add(LeaderboardPane);
     }
 
-    public void switchEventPage() {
-        if (MenuPane.getTranslateX() == 0) {
-            slideInTransition.play();
-        }
-        stackPane.getChildren().clear();
-        stackPane.getChildren().add(EventPane);
-    }
-
-    public void switchCreateEventPage() {
-        if (MenuPane.getTranslateX() == 0) {
-            slideInTransition.play();
-        }
-        stackPane.getChildren().clear();
-        stackPane.getChildren().add(CreateEventPane);
-    }
-
-    public void switchQuizPage() {
-        if (MenuPane.getTranslateX() == 0) {
-            slideInTransition.play();
-        }
-        stackPane.getChildren().clear();
-        stackPane.getChildren().add(QuizPane);
-    }
-
-    public void switchCreateQuizPage() {
-        if (MenuPane.getTranslateX() == 0) {
-            slideInTransition.play();
-        }
-        stackPane.getChildren().clear();
-        stackPane.getChildren().add(CreateQuizPane);
-    }
-
+//    public void switchEventPage() {
+//        if (MenuPane.getTranslateX() == 0) {
+//            slideInTransition.play();
+//        }
+//        stackPane.getChildren().clear();
+//        stackPane.getChildren().add(EventPane);
+//    }
+//
+//    public void switchCreateEventPage() {
+//        if (MenuPane.getTranslateX() == 0) {
+//            slideInTransition.play();
+//        }
+//        stackPane.getChildren().clear();
+//        stackPane.getChildren().add(CreateEventPane);
+//    }
+//
+//    public void switchQuizPage() {
+//        if (MenuPane.getTranslateX() == 0) {
+//            slideInTransition.play();
+//        }
+//        stackPane.getChildren().clear();
+//        stackPane.getChildren().add(QuizPane);
+//    }
+//
+//    public void switchCreateQuizPage() {
+//        if (MenuPane.getTranslateX() == 0) {
+//            slideInTransition.play();
+//        }
+//        stackPane.getChildren().clear();
+//        stackPane.getChildren().add(CreateQuizPane);
+//    }
+//    
+//    public void switchStudentProfilePage() {
+//        if (MenuPane.getTranslateX() == 0) {
+//            slideInTransition.play();
+//        }
+//        stackPane.getChildren().clear();
+//        stackPane.getChildren().add(StudentProfilePane);
+//    }
+    
     // Button effect method
     public void ButtonEffect(Button button) {
         // Create a scale transition
@@ -300,5 +347,47 @@ public class StudentController implements Initializable {
 //                        
 //        //Step 4: add data inside table
 //           myTable.setItems(data);
+    }
+    
+    // Method to add a friend button to the VBox
+    private void addFriendButton(String friendName) {
+        Button friendButton = new Button(friendName);
+        friendButton.setStyle("-fx-background-color: transparent; -fx-text-fill: black; -fx-font-family: \"Segoe UI Semibold\";-fx-font-size: 20px; ");
+        ButtonEffect(friendButton);
+        friendButton.setOnAction(event -> openProfilePage(friendName));
+        FriendListVBox.getChildren().add(friendButton);
+    }
+
+    // Method to open the friend's profile page
+    private void openProfilePage(String friendName) {
+        // open the profile page using the provided name
+        //need to add "add friend" button
+    } 
+    
+    // Method to add an HBox with three buttons to the VBox
+    private void addFriendRequest(String friendName) {
+        // Create an HBox
+        HBox FriendRequestHBox = new HBox();
+        FriendRequestHBox.setSpacing(10); // Spacing between buttons
+
+        // Create three buttons and add them to the HBox
+        Button name = new Button(friendName);
+        Button confirm = new Button("CONFIRM");
+        Button delete = new Button("DELETE");
+
+        name.setStyle("-fx-background-color: transparent; -fx-text-fill: black; -fx-font-family: \"Segoe UI Semibold\";-fx-font-size: 20px; ");
+        name.setPrefWidth(170);
+        
+        // Add custom styles to button2 and button3
+        confirm.getStyleClass().add("confirm");
+        delete.getStyleClass().add("delete");
+        
+        ButtonEffect(name);
+        name.setOnAction(event -> openProfilePage(friendName));
+
+        FriendRequestHBox.getChildren().addAll(name, confirm, delete);
+
+        // Add the HBox to the VBox
+        FriendRequestVBox.getChildren().add(FriendRequestHBox);
     }
 }
